4e8a5de68b3cefcd92b851f7df2b06bab7c06aaf
sjyang1991 <sjyang1991@naver.com>
2020-04-15 18:08:47 +0900

    Update crawling_jobnotice.py

--- crawling_jobnotice.py
+++ crawling_jobnotice.py
@@ -21,20 +21,24 @@ source = json.loads(response.text)
 results = ''
 i = 1
 recentNum = 0
+notice_up_yn = 0
 print('최근 게시물 번호'+str(recentNum))
 if source['list'][0]['jobnoticeSn'] != recentNum:
     recentNum = source['list'][0]['jobnoticeSn']
     results = '----- 관심 채용 정보 -----\n\n'
     for row in source["list"]:
-        if row['jobnoticeName'].find('전산원') != -1 or row['jobnoticeName'].find('정보보안팀') != -1:
-            results += '전산원' + row['receiptState'] + '\n'
+     #   if row['jobnoticeName'].find('전산원') != -1 or row['jobnoticeName'].find('정보보안팀') != -1:
+     #       results += '전산원' + row['receiptState'] + '\n'
 
-        if row['receiptState'] == '접수중' :
+        if row['receiptState'] == '접수중' AND (row['jobnoticeName'].find('전산원') != -1 or row['jobnoticeName'].find('정보보안팀') != -1) :
             results += str(i) + '. ' + str(row['jobnoticeSn']) + ' ' + row['recruitTypeName'] + '\n ' + row['jobnoticeName']+ ' ' + row['receiptState'] + '\n ' + str(datetime.date.fromtimestamp(row['applyStartDate']['time']/1000))+ ' ~ ' + str(datetime.datetime.fromtimestamp(row['applyEndDate']['time']/1000))+ ' \n' + "https://yuhs.recruiter.co.kr/app/jobnotice/view?systemKindCode="+row['systemKindCode']+'&jobnoticeSn='+str(row['jobnoticeSn']) + '\n\n' 
             i += 1
-print(results)
-if results != '새 공고 없음':
+            notice_up_yn = 1
+            
+if notice_up_yn = 1 :
     send(results)
-results = '새 공고 없음'
+else :
+    results += '전산 공고 없음\n'
+print(results)
 
     
